2130dd96ccede709bd36977354795143e0788dc8
MrNikhillYadav <facts.foundr@gmail.com>
2025-08-19 21:54:10 +0530

    add Week 19

---
+++ Web-Dev/Aug-19/script.js
@@ -0,0 +1,38 @@
+
+
+// let username = "John"
+// console.log(username)
+
+// creating a JS object:
+
+// let student = {
+//     name : "Ram",
+//     age : 20,
+//     class : 12
+//  }
+
+//  console.log(student)
+//  console.log(student.age)
+//  console.log(student.class)
+//  console.log(student.name)
+
+// JS Operators
+
+let x = 50;
+let y = 10;
+
+let sum = x + y;
+console.log( sum);
+
+let multiply = x * y;
+console.log(multiply);
+
+let subtraction = x - y;
+console.log(subtraction);
+
+let division = x / y;
+console.log(division)
+
+
+
+
