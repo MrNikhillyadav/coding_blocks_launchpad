d36c2da1fdc05edf8a566c30be092501147a4ee5
MrNikhillYadav <facts.foundr@gmail.com>
2025-09-08 21:08:29 +0530

    init_sep_09

---
+++ Web-Dev/Sep-09/script.js
@@ -0,0 +1,13 @@
+// DOM Manipulation
+
+
+// Accessing elements :
+
+let btn1 = document.getElementById('btn1');
+console.log(btn1);
+
+let title = document.getElementById('title')
+console.log('title: ', title);
+
+let desc = document.getElementsByClassName('description')
+console.log('desc: ', desc[0]);
