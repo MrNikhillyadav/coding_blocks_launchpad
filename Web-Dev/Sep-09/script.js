4ceb9ab509fd2777ffe1e1b79e2381a46fb7c9fe
MrNikhillYadav <facts.foundr@gmail.com>
2025-09-09 16:05:56 +0530

    add Sep_09 class

--- Web-Dev/Sep-09/script.js
+++ Web-Dev/Sep-09/script.js
@@ -3,7 +3,7 @@
 
 // Accessing elements :
 
-let btn1 = document.getElementById('btn1');
+// let btn1 = document.getElementById('btn1');
 console.log(btn1);
 
 let title = document.getElementById('title')
@@ -11,3 +11,58 @@ console.log('title: ', title);
 
 let desc = document.getElementsByClassName('description')
 console.log('desc: ', desc[0]);
+
+
+// queryselector
+
+let title = document.querySelector("#title")
+console.log(title);
+
+let desc = document.querySelector(".description")
+console.log(desc);
+
+
+
+// addEventListener
+
+let btn3 = document.querySelector("#btn3");
+console.log(btn3);
+
+btn3.addEventListener("click",function(){
+    console.log("btn3 is clicked");
+})
+
+let btn2 = document.querySelector("#btn2");
+
+btn2.addEventListener("mouseover",function(){
+    console.log("mouse over btn2");
+})
+
+
+
+
+// Modifying / Manipulation DOM
+
+// let title = document.querySelector("#title");
+console.log('title: ', title);
+
+// // modify the text inside an element
+title.innerHTML = "Apple";
+
+// // access
+let btn1 = document.querySelector("#btn1");
+btn1.innerText = "submit";
+
+
+title.style.backgroundColor = "yellow"
+title.style.padding = "20px"
+
+
+
+
+
+
+
+
+
+
