70bbb222f5ce648d0b961916ff793af3e52bfdf4
MrNikhillYadav <facts.foundr@gmail.com>
2025-09-13 16:39:57 +0530

    add

--- DSA/Sep-12/program2.cpp
+++ DSA/Sep-12/program2.cpp
@@ -1,9 +1,22 @@
+/* 
+    Given an infinite stream of non-negative integers, design an alogithm to output the top-k
+    integers every time you encounter -1;
+
+    Example:
+        Input : 4,1,2,-1,3,-1,7,0,1,2,-1,8,-1,.....;   k=3
+        output: 1 2 4
+                2 3 4
+                3 4 7 
+                4 7 8
+                . . .
+*/
+
 #include <iostream>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
-//printHeap
 void printHeap(priority_queue<int, vector<int>, greater<int>> minHeap){
     while(!minHeap.empty()){
         cout << minHeap.top() << " ";
@@ -14,12 +27,16 @@ void printHeap(priority_queue<int, vector<int>, greater<int>> minHeap){
 
 
 int main(){
+
     int k = 3;
+
+    //creating a min heap
     priority_queue<int, vector<int>, greater<int>>minHeap;
 
     for(int i = 0;  i < k; i++){
         int val;
         cin >> val;
+
         minHeap.push(val);
     }
 
